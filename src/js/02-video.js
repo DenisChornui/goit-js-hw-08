cedc59b2f5851f4545ae80e7992f4adf6555853a
DenisChornui <129185799+DenisChornui@users.noreply.github.com>
2023-08-24 19:02:55 +0300

    comtaska2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const iframePlayer = new Player(iframe);
+
+
+iframePlayer.on('timeupdate', throttle(playerTime, 1000));
+
+function playerTime(data) {
+    console.log(data);
+    let seconds = data.seconds;
+    console.log(seconds);
+    localStorage.setItem('videoplayer-current-time', seconds);
+};
+
+const currentSeconds = localStorage.getItem('videoplayer-current-time');
+console.log(currentSeconds)
+iframePlayer.setCurrentTime(currentSeconds);
+
 
